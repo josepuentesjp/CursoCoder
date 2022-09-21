3dcec63726cae54e831e62f9f8c5e921ed878a10
José Puentes <josepuentesjp@gmail.com>
2022-09-20 22:12:01 -0300

    Update views.py

--- MVT_JosePuentes/App_family/views.py
+++ MVT_JosePuentes/App_family/views.py
@@ -11,10 +11,10 @@ def familia(self):
 
     mother = family(nombre = 'Yasmely', years = 2022 - 1965, born_in = '1965-04-05',  hobbie = 'Ir a la iglesia')
     mother.save()
-    show2 = f'El nombre de mi padre es {mother.nombre} tiene {mother.years} de edad, nació el {mother.born_in} y su hobbie es {mother.hobbie}.'
+    show2 = f'El nombre de mi madre es {mother.nombre} tiene {mother.years} de edad, nació el {mother.born_in} y su hobbie es {mother.hobbie}.'
     
     sister = family(nombre = 'Rosmely', years = 2022 - 1999, born_in = '1999-08-29',  hobbie = 'Ver series')
     sister.save()
-    show3 = f'El nombre de mi padre es {sister.nombre} tiene {sister.years} de edad, nació el {sister.born_in} y su hobbie es {sister.hobbie}.'
+    show3 = f'El nombre de mi hermana es {sister.nombre} tiene {sister.years} de edad, nació el {sister.born_in} y su hobbie es {sister.hobbie}.'
 
     return HttpResponse(f'{show} <br> {show2} <br> {show3}')
